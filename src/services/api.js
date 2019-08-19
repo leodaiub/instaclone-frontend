bcfc3d2145a62f638a211e157e312d03039d2294
Leonardo Daiub <45826152+leodaiub@users.noreply.github.com>
2019-08-19 18:16:47 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3030',
+    baseURL: 'https://leodaiub-instaclonebackend.netlify.com',
 });
 
 export default api;
